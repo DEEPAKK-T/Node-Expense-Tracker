0bca0802b0816108e54ab0433b1968c171d52c5f
DeepakKt <deepakkt2032@gmail.com>
2023-11-09 22:42:16 +0530

    update:data and req methods setup

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var http = require("http")
+// require("dotenv").config()
 
-http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Hello server");
-}).listen(8080);
+const PORT = process.env.PORT || 8080
+
+http.createServer(function (req, res) {
+    // res.writeHead(200, {"Content-Type":"text/plain"});
+    // res.end("Hello server");
+
+    switch (req.method) {
+        case "GET":
+            getRequest(req, res);
+            break;
+        case "POST":
+            postRequest(req, res);
+            break;
+        case "PUT":
+            putRequest(req, res);
+            break;
+        case "DELETE":
+            deleteRequest(req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json")
+            res.write(JSON.stringify({ title: "Not Found", message: "Endpoint not found" }));
+            res.end()
+
+    }
+}).listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
